5f0ba516dfdda688b1f0b470a46d000b01f6f956
Gabe L <gabe@fijiwebdesign.com>
2016-07-26 17:39:46 -0500

    Added debugging to karma via chrome

--- spec.js
+++ spec.js
@@ -4,5 +4,10 @@ require('babel-core/polyfill');
 require('./vendor/angular.src');
 require('angular-mocks');
 
+require('./src/app.js')
+var test = 'ok'
+
+debugger;
+
 testsContext = require.context('./src', true, /_spec\.js$/);
 testsContext.keys().forEach(testsContext);
